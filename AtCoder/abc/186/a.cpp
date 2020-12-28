03a03cf919747db2ba33f1b961bfb3843c16e2ba
Gourav Khunger <gouravkhunger18@gmail.com>
2020-12-28 22:32:51 +0530

    Create a.cpp

---
+++ AtCoder/abc/186/a.cpp
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc186/tasks/abc186_a
+// Brick
+// File Creation Date: 28-Dec-2020
+// Author: Gourav(https://github.com/GouravKhunger)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main() {
+    FIO;
+    int n, w;
+    cin>>n>>w;
+    cout<<n/w;
+    return 0;
+}
